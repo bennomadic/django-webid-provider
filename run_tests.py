ae64820d3517d8d078916fde966ac30cefd7ee33
Ben Carrillo <bennomadic@gmail.com>
2011-12-20 23:46:00 -0400

    tests running programatically

---
+++ run_tests.py
@@ -0,0 +1,37 @@
+import sys, os
+
+from django.conf import settings
+from django.conf.urls.defaults import patterns, include
+from django.core.management import call_command
+
+setup_root = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(setup_root, "src"))
+sys.path.insert(0, os.path.join(setup_root, "examples"))
+
+
+def main():
+    # Dynamically configure the Django settings with the minimum necessary to
+    # get Django running tests
+    settings.configure(
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.staticfiles',
+            'uni_form',
+            'django_webid.provider',
+        ),
+        # Django replaces this, but it still wants it. *shrugs*
+        DATABASE_ENGINE='sqlite3',
+        SITE_ID=1,
+        AUTH_PROFILE_MODULE = 'django_webid.provider.WebIDBasicProfile',
+        ROOT_URLCONF = 'example_webid_provider.urls',
+
+    )
+
+    # Fire off the tests
+    call_command('test', 'provider')
+
+if __name__ == '__main__':
+    main()
